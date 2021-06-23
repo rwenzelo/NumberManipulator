21d5059466505c7536f43e24efa83438be36b211
Wenzelow <32124@nelnet.net>
2021-06-23 10:19:12 -0500

    consuleExample

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumFromScreen = Convert.ToInt32(Console.ReadLine()) ;
+            NumFromScreen = (NumFromScreen * NumFromScreen) + 14 - 3;
+            Console.WriteLine(NumFromScreen);
+        }
+    }
+}
